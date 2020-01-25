7eb0f8cf1737f234ccc6ce7dd805f0f6244746f5
noyuno <noyuno@users.noreply.github.com>
2020-01-25 18:39:45 +0900

    a

--- EPGStation/enc.sh
+++ EPGStation/enc.sh
@@ -9,3 +9,4 @@ nice -n 10 "$FFMPEG" -y  -dual_mono_mode main -i "$INPUT" \
     -movflags faststart -map 0 -ignore_unknown -max_muxing_queue_size 1024 -sn \
     -vf yadif=0:-1:1 -preset veryfast -aspect 16:9 $res \
     -c:v libx264 -crf 23 -c:a aac -ar 48000 -ab 192k -ac 2 -tune animation,fastdecode,zerolatency "$OUTPUT"
+chmod 777 "$OUTPUT"
