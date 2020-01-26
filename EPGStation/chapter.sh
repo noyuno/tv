a4ffa7c239198e55ac5f78c87175d05671cda09a
noyuno <noyuno@users.noreply.github.com>
2020-01-26 21:38:34 +0900

    a

---
+++ EPGStation/chapter.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+tmpd=$(mktemp -d)
+nice -n 10 comskip --ini /home/noyuno/tv/comskip.ini --output=$tmpd --output-filename=comskip "$OUTPUT"
+c=1
+printf "CHAPTER%02d=0:00:00.00\n" $c >> $tmpd/comskip.chp
+printf "CHAPTER%02dNAME=%02d-program\n" $c $c >> $tmpd/comskip.chp
+while read line ; do
+  c=$((c+1))
+  if [ $(echo "$line" | awk '{print $2}') = 'start' ]; then
+    t='cm'
+  else
+    t='program'
+  fi
+  printf "CHAPTER%02d=%s\n" $c $(echo "$line" | awk '{print $1}') >> $tmpd/comskip.chp
+  printf "CHAPTER%02dNAME=%02d-%s\n" $c $c $t >> $tmpd/comskip.chp
+done < $tmpd/comskip.vdr
+
+nice -n 10 MP4Box -chap $tmpd/comskip.chp "$OUTPUT"
+
+rm -rf "$tmpd"
--- EPGStation/enc.sh
+++ EPGStation/enc.sh
@@ -10,23 +10,6 @@ nice -n 10 "$FFMPEG" -y  -dual_mono_mode main -i "$INPUT" \
     -vf bwdif=0:-1:1 -preset veryfast -aspect 16:9 $res \
     -c:v libx264 -crf 23 -coder 1 -c:a aac -ar 48000 -ab 192k -ac 2 -tune zerolatency "$OUTPUT"
 
-tmpd=$(mktemp -d)
-nice -n 10 comskip --ini /home/noyuno/tv/comskip.ini --output=$tmpd --output-filename=comskip "$OUTPUT"
-c=1
-printf "CHAPTER%02d=0:00:00.00\n" $c >> $tmpd/comskip.chp
-printf "CHAPTER%02dNAME=%02d-program\n" $c $c >> $tmpd/comskip.chp
-while read line ; do
-  c=$((c+1))
-  if [ $(echo "$line" | awk '{print $2}') = 'start' ]; then
-    t='cm'
-  else
-    t='program'
-  fi
-  printf "CHAPTER%02d=%s\n" $c $(echo "$line" | awk '{print $1}') >> $tmpd/comskip.chp
-  printf "CHAPTER%02dNAME=%02d-%s\n" $c $c $t >> $tmpd/comskip.chp
-done < $tmpd/comskip.vdr
+OUTPUT="$OUTPUT" bash /home/noyuno/tv/EPGStation/chapter.sh
 
-nice -n 10 MP4Box -chap $tmpd/comskip.chp "$OUTPUT"
-
-rm -rf "$tmpd"
 chmod 777 "$OUTPUT"
