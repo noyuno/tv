6e5725135bd839795edfe227865adc64a2fdc9ca
noyuno <noyuno@users.noreply.github.com>
2020-01-26 20:36:52 +0900

    a

--- EPGStation/enc.sh
+++ EPGStation/enc.sh
@@ -5,15 +5,13 @@ case "$VIDEORESOLUTION" in
   "480p" ) res="-s 960x480" ;;
 esac
 
-mp4tmp="${OUTPUT%.*}-tmp.mp4"
-
 nice -n 10 "$FFMPEG" -y  -dual_mono_mode main -i "$INPUT" \
     -movflags +faststart -map 0 -ignore_unknown -max_muxing_queue_size 1024 -sn \
     -vf bwdif=0:-1:1 -preset veryfast -aspect 16:9 $res \
-    -c:v libx264 -crf 23 -coder 1 -c:a aac -ar 48000 -ab 192k -ac 2 -tune zerolatency "$mp4tmp"
+    -c:v libx264 -crf 23 -coder 1 -c:a aac -ar 48000 -ab 192k -ac 2 -tune zerolatency "$OUTPUT"
 
 tmpd=$(mktemp -d)
-nice -n 10 comskip --ini /home/noyuno/tv/comskip.ini --output=$tmpd --output-filename=comskip "$mp4tmp"
+nice -n 10 comskip --ini /home/noyuno/tv/comskip.ini --output=$tmpd --output-filename=comskip "$OUTPUT"
 c=0
 while read line ; do
   c=$((c+1))
@@ -26,8 +24,7 @@ while read line ; do
   printf "CHAPTER%02dNAME=%02d-%s\n" c c t >> $tmpd/comskip.chp
 done < $tmpd/comskip.vdr
 
-nice -n 10 MP4Box -chap $tmpd/comskip.chp -out "$OUTPUT" "$mp4tmp"
+nice -n 10 MP4Box -chap $tmpd/comskip.chp "$OUTPUT"
 
 rm -rf "$tmpd"
-rm "$mp4tmp"
 chmod 777 "$OUTPUT"
