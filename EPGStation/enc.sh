4b8cb61b0f4d8d725eca18fc99ad935d3cb7cb45
noyuno <noyuno@users.noreply.github.com>
2020-01-26 13:21:43 +0900

    a

--- EPGStation/enc.sh
+++ EPGStation/enc.sh
@@ -6,7 +6,7 @@ case "$VIDEORESOLUTION" in
 esac
 
 nice -n 10 "$FFMPEG" -y  -dual_mono_mode main -i "$INPUT" \
-    -movflags faststart -map 0 -ignore_unknown -max_muxing_queue_size 1024 -sn \
-    -vf yadif=0:-1:1 -preset veryfast -aspect 16:9 $res \
-    -c:v libx264 -crf 23 -c:a aac -ar 48000 -ab 192k -ac 2 -tune animation,zerolatency "$OUTPUT"
+    -movflags +faststart -map 0 -ignore_unknown -max_muxing_queue_size 1024 -sn \
+    -vf bwdif=0:-1:1 -preset veryfast -aspect 16:9 $res \
+    -c:v libx264 -crf 23 -coder 1 -c:a aac -ar 48000 -ab 192k -ac 2 -tune animation,zerolatency "$OUTPUT"
 chmod 777 "$OUTPUT"
