a76ad85c3678f5764963bafbec95ab77a27e9a1b
noyuno <noyuno@users.noreply.github.com>
2020-02-22 16:27:17 +0900

    a

--- EPGStation/notifyenc.sh
+++ EPGStation/notifyenc.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
 source /home/noyuno/tv/.env
-mes="$1:\\\\n" \
-"RECORDEDID=$RECORDEDID\\\\n" \
-"CHANNELID=$CHANNELID\\\\n" \
-"VIDEORESOLUTION=$VIDEORESOLUTION\\\\n" \
-"NAME=$NAME\\\\n" \
-"OUTPUT=$OUTPUT"
+mes="$1:\\\\n\
+RECORDEDID=$RECORDEDID\\\\n\
+CHANNELID=$CHANNELID\\\\n\
+VIDEORESOLUTION=$VIDEORESOLUTION\\\\n\
+NAME=$NAME\\\\n\
+OUTPUT=$OUTPUT"
 curl -s -X POST -H "Content-Type: application/json" -d '{"value1":"'$mes'"}' "https://maker.ifttt.com/trigger/m1/with/key/$IFTTTKEY"
