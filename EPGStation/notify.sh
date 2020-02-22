1ca672005b6c940aaafdad0908ab3c262c34a83e
noyuno <noyuno@users.noreply.github.com>
2020-02-22 19:32:26 +0900

    a

--- EPGStation/notify.sh
+++ EPGStation/notify.sh
@@ -11,13 +11,13 @@ if [ "$DURATION" ]; then
   du=$(echo $DURATION/1000/60 | bc)
 fi
 mes="$1:\\\\n\
-  RECORDEDID=$RECORDEDID\\\\n\
-  PROGRAMID=$PROGRAMID\\\\n\
-  CHANNELTYPE=$CHANNELTYPE\\\\n\
-  CHANNELID=$CHANNELID\\\\n\
-  CHANNELNAME=$CHANNELNAME\\\\n\
-  STARTAT=$st\\\\n\
-  ENDAT=$en\\\\n\
-  DURATION=$du\\\\n\
-  NAME=$NAME"
+    RECORDEDID=$RECORDEDID\\\\n\
+    PROGRAMID=$PROGRAMID\\\\n\
+    CHANNELTYPE=$CHANNELTYPE\\\\n\
+    CHANNELID=$CHANNELID\\\\n\
+    CHANNELNAME=$CHANNELNAME\\\\n\
+    STARTAT=$st\\\\n\
+    ENDAT=$en\\\\n\
+    DURATION=$du\\\\n\
+    NAME=$NAME"
 curl -s -X POST -H "Content-Type: application/json" -d '{"value1":"'"$mes"'"}' "https://maker.ifttt.com/trigger/m1/with/key/$IFTTTKEY"
