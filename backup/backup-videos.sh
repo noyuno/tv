ae24c2d169382479d6b1095d3a24fc41734ac2e7
noyuno <noyuno@users.noreply.github.com>
2020-05-17 22:32:26 +0900

    a

--- backup/backup-videos.sh
+++ backup/backup-videos.sh
@@ -1,16 +1,26 @@
 #!/bin/bash -e
+set -e
 
 source /home/noyuno/tv/.env
 src=/mnt/hdd/mp4
 dest=/mnt/data/m2mp4
+list=/mnt/hdd/mp4/00-backup-list.txt
 mkdir -p $dest
 
+if [ ! -d $src ]; then
+  echo 'src directory not found, $src='$src 1>&2
+  exit 1
+fi
+if [ ! -f $list ]; then
+  echo 'file list not found, $list='$list 1>&2
+  exit 1
+fi
 # videos
 
 pushd $src
 while read line ; do
   find . -type f | grep "$line" >> /tmp/backup-videos-list
-done < /mnt/hdd/mp4/00-backup-list.txt
+done < $list
 popd
 
 rsync -avh --files-from=/tmp/backup-videos-list $src/ $dest
