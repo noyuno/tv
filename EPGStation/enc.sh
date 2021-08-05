da570061f9d18f4de3f3efa36245d1c6c678cb44
noyuno <noyuno@users.noreply.github.com>
2021-08-05 20:38:53 +0900

    a

--- EPGStation/enc.sh
+++ EPGStation/enc.sh
@@ -20,7 +20,7 @@ echo OUTPUT=$OUTPUT
 nice -n 10 "$FFMPEG" -y  -dual_mono_mode main -i "$INPUT" \
     -movflags +faststart -map 0 -ignore_unknown -max_muxing_queue_size 1024 -sn \
     -vf "$vf" -preset veryfast -aspect 16:9 \
-    -c:v libx264 -crf "$crf" -coder 1 -c:a aac -ar 48000 -ab 192k -ac 2 -loglevel warning "$OUTPUT" < /dev/null
+    -c:v libx264 -crf "$crf" -coder 1 -c:a aac -ar 48000 -ab 192k -ac 2 "$OUTPUT" < /dev/null
 
 OUTPUT="$OUTPUT" bash /home/noyuno/tv/EPGStation/chapter.sh
 
