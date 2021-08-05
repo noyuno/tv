8c254f292ce8cc36ff875082082e522cfce3a5e4
noyuno <noyuno@users.noreply.github.com>
2021-08-05 19:23:00 +0900

    a

--- EPGStation/enc.sh
+++ EPGStation/enc.sh
@@ -28,4 +28,4 @@ dirname "$OUTPUT" | xargs chmod 777
 chmod 777 "$OUTPUT"
 
 bash /home/noyuno/tv/EPGStation/notifyenc.sh ':coffee: エンコードが完了しました' &&:
-) | tee /home/noyuno/EPGStation/logs/enc.sh.log
+) | tee -a /home/noyuno/EPGStation/logs/enc.sh.log
